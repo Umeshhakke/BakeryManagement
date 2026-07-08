c39bfc44bd692bde18ebabb3001564694835881c
Umesh Hakke <umeshhakke050@gmail.com>
2026-07-09 02:28:55 +0530

    backend connection

--- bakery_frontend/src/services/api.js
+++ bakery_frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api', // This points to your Django backend
+  baseURL: 'https://bakerymanagement-yjcx.onrender.com/api', // This points to your Django backend
 });
 
 export default API;
